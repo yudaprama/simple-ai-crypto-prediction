6b0797feb43d71d2be83d5195b4c2154c5636553
yuda <yudhaprama@icloud.com>
2017-12-08 10:35:56 +0700

    get fetcher

--- predict.py
+++ predict.py
@@ -35,7 +35,7 @@ def chart(real,predicted,show=True):
     plt.plot(real,color='g')
     plt.plot(predicted,color='r')
     plt.ylabel('BTC/USD')
-    plt.xlabel("9Minutes")
+    plt.xlabel("9 Minutes")
     plt.savefig("chart.png")
     if show:plt.show()
 
